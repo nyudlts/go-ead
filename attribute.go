826c80344aaebf7b78b1c4cd900b6a32f8edda9c
Donald Mennerich <don.mennerich@nyu.edu>
2020-07-08 18:52:50 -0400

    removing stdout messages from atttributes.go

--- attribute.go
+++ attribute.go
@@ -9,19 +9,16 @@ func ContainsInternalAudienceAttr(faid []byte) (bool, error) {
 	default_result := false
 	doc, err := libxml2.Parse(faid)
 	if err != nil {
-		//fmt.Printf("Failed to parse ead: %s", err)
 		return default_result, err
 	}
 	defer doc.Free()
 	root, err := doc.DocumentElement()
 	ctx, err := xpath.NewContext(root)
 	if err != nil {
-		//fmt.Printf("Failed to initialize XPathContext: %s", err)
 		return default_result, err
 	}
 	defer ctx.Free()
 	if err := ctx.RegisterNS(prefix, nsuri); err != nil {
-		//fmt.Printf("Failed to initialize XPathContext: %s", err)
 		return default_result, err
 	}
 	n := xpath.NodeList(ctx.Find(`//ead:*[@audience="internal"]`))
